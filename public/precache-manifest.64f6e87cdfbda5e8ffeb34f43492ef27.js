d97e95517053b786485a19b67a6388a8e6ac4b75
Miguel Gomez <aster.december@gmail.com>
2019-02-10 05:15:17 -0600

    setting react in back

---
+++ public/precache-manifest.64f6e87cdfbda5e8ffeb34f43492ef27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da63886ebf4ece333726",
+    "url": "/static/js/main.da63886e.chunk.js"
+  },
+  {
+    "revision": "82da77665de4eb384098",
+    "url": "/static/js/1.82da7766.chunk.js"
+  },
+  {
+    "revision": "da63886ebf4ece333726",
+    "url": "/static/css/main.bd52c21b.chunk.css"
+  },
+  {
+    "revision": "82da77665de4eb384098",
+    "url": "/static/css/1.ea9c4352.chunk.css"
+  },
+  {
+    "revision": "9fb2a2bd4147f91655201e33855bb2a8",
+    "url": "/index.html"
+  }
+];
